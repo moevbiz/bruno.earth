228a9d2013613724252a57fa7cf72a920c00e187
Bruno <moev.biz@moev.biz>
2024-03-26 23:07:48 +0100

    new earth

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const iframe = document.querySelector('body');
-    iframe.addEventListener('click', e => {
-        // reloads iframe
-        e.target.querySelector('iframe').src += '';
-    })
+    
 })
